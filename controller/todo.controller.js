efc0cf2cf3ec4ef0c978f54546dbe13e24c0af57
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-06 12:39:03 +0300

    Update todo.controller.js

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -8,18 +8,29 @@ class todoController {
         res.status(201);
         res.json(newTask.rows);
     }
-
-    async getTask(req,res){
-
+    async getTasks(req,res) {
+        const tasks = await db.query(`SELECT * FROM todo`)
+        res.status(200);
+        res.json(tasks.rows)
     }
     async getOneTask(req,res){
-
+        const id = req.params.id
+        const task = await db.query(`SELECT * FROM todo where id = $1`, [id])
+        res.status(200);
+        res.json(task.rows[0])
     }
     async updateTask(req,res){
-
+        const {id, title, done, due_date} = req.body
+        const taskUpdate = await db.query(`UPDATE todo SET title =  $2, done = $3, due_date = $4 where id = $1 RETURNING *`,
+            [id, title, done, due_date])
+        res.status(200);
+        res.json(taskUpdate.rows[0])
     }
     async deleteTask(req,res){
-
+        const id = req.params.id
+        const task = await db.query(`DELETE FROM todo where id = $1`, [id])
+        res.status(204);
+        res.json(task.rows[0])
     }
 }
 module.exports = new todoController();
