5e7f440711774df9fc93449655e60b0568213b47
bromanovich99 <73001876+bromanovich99@users.noreply.github.com>
2021-08-06 11:44:47 +0300

    update post task

--- controller/todo.controller.js
+++ controller/todo.controller.js
@@ -2,9 +2,9 @@ const db = require('../db')
 
 class todoController {
 
-    async createTask (req,res) {
-        const {title, done, due_date} = req.body
-        const newTask = await db.query(`INSERT INTO todo (title, done, due_date) VALUES ($1, $2, $3) RETURNING *`, [title, done, due_date]);
+    async createTask(req, res) {
+        const { id, title, done, due_date } = req.body
+        const newTask = await db.query(`INSERT INTO todo (id, title, done, due_date) VALUES ($1, $2, $3, $4) RETURNING *`, [id, title, done, due_date]);
         res.status(201);
         res.json(newTask.rows);
     }
